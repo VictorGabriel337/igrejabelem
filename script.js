90e02b57faca12fbeebe4a649f61c2aacf9d6914
Victor Souza <victor.abrao337@gmail.com>
2025-08-14 08:55:41 -0300

    alterado 14.08

--- script.js
+++ script.js
@@ -130,6 +130,7 @@ document.addEventListener("DOMContentLoaded", () => {
           foto: fotoBase64
         };
 
+        const enderecoCompleto = document.getElementById("endereco").value + " " + document.getElementById("Num-endereco").value;
         try {
           const resposta = await fetch(`${backendURL}/cadastrar`, {
             method: "POST",
@@ -193,8 +194,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <div class="social-media">
                   <i class="fa-solid fa-phone"></i>
                   <a href="https://wa.me/${cadastro.telefone}" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
-                  <a href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(cadastro.endereco + ' ' + (cadastro.numero || ''))}" target="_blank"><i class="fa-solid fa-house"></i></a>
-                </div>
+                 <a href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(enderecoCompleto)}" target="_blank"><i class="fa-solid fa-house"></i></a>
                 <div class="name-profession">
                   <span class="name">${cadastro.nome?.split(" ")[0]}</span>
                   <span class="cargo">${cadastro.cargo || "Membro"}</span>
